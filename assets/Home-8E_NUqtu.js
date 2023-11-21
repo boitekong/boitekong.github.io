d468524f93d93dc03c9528bcd1c566bb77e1ed24
boitekong <148322598+boitekong@users.noreply.github.com>
2023-11-21 14:04:05 +0200

    updates

---
+++ assets/Home-8E_NUqtu.js
@@ -0,0 +1,7 @@
+import{_ as a}from"./index-sCKf3Kq7.js";import{R as e,r as t}from"./vendor-m6_awznP.js";const r=t.lazy(()=>a(()=>import("./Main-S8DXDp5J.js"),__vite__mapDeps([0,1,2,3,4]))),l=t.lazy(()=>a(()=>import("./Nav-aroZJ-vj.js"),__vite__mapDeps([5,1])));function m(){return e.createElement(e.Fragment,null,e.createElement(t.Suspense,{className:"loadingDiv"},e.createElement(r,null),e.createElement(l,null)))}export{m as default};
+function __vite__mapDeps(indexes) {
+  if (!__vite__mapDeps.viteFileDeps) {
+    __vite__mapDeps.viteFileDeps = ["assets/Main-S8DXDp5J.js","assets/vendor-m6_awznP.js","assets/share-GLoeCvUs.js","assets/index-sCKf3Kq7.js","assets/index-DG5PA7ap.css","assets/Nav-aroZJ-vj.js"]
+  }
+  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
+}
