96ade6d506dbc4b6f65516213cf03c803d6fe14a
boitekong <148322598+boitekong@users.noreply.github.com>
2024-01-24 18:06:01 +0200

    daily updates

---
+++ assets/Home-aVLCCCbF.js
@@ -0,0 +1,7 @@
+import{_ as a}from"./index-WqMIF6lp.js";import{R as e,r as t}from"./vendor-ZswFYO6G.js";const r=t.lazy(()=>a(()=>import("./Main-iw-k-ewg.js"),__vite__mapDeps([0,1,2,3,4]))),l=t.lazy(()=>a(()=>import("./Nav-NBLUj82w.js"),__vite__mapDeps([5,1])));function m(){return e.createElement(e.Fragment,null,e.createElement(t.Suspense,{className:"loadingDiv"},e.createElement(r,null),e.createElement(l,null)))}export{m as default};
+function __vite__mapDeps(indexes) {
+  if (!__vite__mapDeps.viteFileDeps) {
+    __vite__mapDeps.viteFileDeps = ["assets/Main-iw-k-ewg.js","assets/vendor-ZswFYO6G.js","assets/share-GLoeCvUs.js","assets/index-WqMIF6lp.js","assets/index-DG5PA7ap.css","assets/Nav-NBLUj82w.js"]
+  }
+  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
+}
