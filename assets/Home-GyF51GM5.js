e6fb40305ad3c8ee0f05c2dc1cf63d7f0bf756b1
boitekong <148322598+boitekong@users.noreply.github.com>
2024-03-12 13:06:23 +0200

    Add files via upload

---
+++ assets/Home-GyF51GM5.js
@@ -0,0 +1,7 @@
+import{_ as a}from"./index-69xK7l6p.js";import{R as e,r as t}from"./vendor-ZswFYO6G.js";const r=t.lazy(()=>a(()=>import("./Main-IB3prN2C.js"),__vite__mapDeps([0,1,2,3,4]))),l=t.lazy(()=>a(()=>import("./Nav-NBLUj82w.js"),__vite__mapDeps([5,1])));function m(){return e.createElement(e.Fragment,null,e.createElement(t.Suspense,{className:"loadingDiv"},e.createElement(r,null),e.createElement(l,null)))}export{m as default};
+function __vite__mapDeps(indexes) {
+  if (!__vite__mapDeps.viteFileDeps) {
+    __vite__mapDeps.viteFileDeps = ["assets/Main-IB3prN2C.js","assets/vendor-ZswFYO6G.js","assets/share-GLoeCvUs.js","assets/index-69xK7l6p.js","assets/index-DG5PA7ap.css","assets/Nav-NBLUj82w.js"]
+  }
+  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
+}
