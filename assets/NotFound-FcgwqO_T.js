4e0747f9a552851d2b6b96dcd9602db10807cd1b
boitekong <148322598+boitekong@users.noreply.github.com>
2024-07-24 09:11:20 +0200

    Add files via upload

---
+++ assets/NotFound-FcgwqO_T.js
@@ -0,0 +1,1 @@
+import{u as e,r as a}from"./vendor-AJt8eDY0.js";function u(){let t=e();return a.useEffect(()=>{t("/")},[t]),null}export{u as default};
---
+++ assets/share-GLoeCvUs.js
@@ -0,0 +1,1 @@
+async function e(r){try{await navigator.share(r)}catch(a){console.error(a)}}export{e as S};
